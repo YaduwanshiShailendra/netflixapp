441255a15023bbf59f0a18a8d5d062b097b39931
shailendra <shailendran70@gmail.com>
2020-08-26 13:50:32 +0530

    Updates

---
+++ precache-manifest.5fd12ee325db2576e76b83aed793e5e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdcfb631bfbc6c0f0f1c3326fa18c628",
+    "url": "/netflixapp/index.html"
+  },
+  {
+    "revision": "2adf2ac1f7e38c871d3f",
+    "url": "/netflixapp/static/css/main.88bb9639.chunk.css"
+  },
+  {
+    "revision": "e62a2ca5830893795f6a",
+    "url": "/netflixapp/static/js/2.b0ac3c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflixapp/static/js/2.b0ac3c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2adf2ac1f7e38c871d3f",
+    "url": "/netflixapp/static/js/main.870b9837.chunk.js"
+  },
+  {
+    "revision": "9b177b9c457bc219171d",
+    "url": "/netflixapp/static/js/runtime-main.2086ea6b.js"
+  }
+]);
